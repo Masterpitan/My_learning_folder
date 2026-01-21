e7d91e6b5bc225f94080768ac3713091788f9d12
Masterpitan <akoredeadetunji93@gmil.com>
2026-01-21 12:34:29 +0100

    Initial commit: Setting local commit of my RAGAPI

---
+++ embed.py
@@ -0,0 +1,12 @@
+# to embed our texts so they can generate answers from stored embeddings
+import chromadb
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_or_create_collection("docs")
+
+with open("k8s.txt", "r") as f:
+    text = f.read()
+collection.add(
+    documents=[text],
+    ids=["k8s"]
+)
+print("Embeddings stored in Chroma")
